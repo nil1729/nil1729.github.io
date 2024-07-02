abd8530d6ad372038afe5075d5cd1b7f9a207a31
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2024-07-03 03:32:21 +0530

    release(fix): vercel deployment fixes (#7)
    * vercel deploy fix
    
    * vercel deploy fix
    
    * vercel deploy fix

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   optimizeFonts: false,
 });
 
